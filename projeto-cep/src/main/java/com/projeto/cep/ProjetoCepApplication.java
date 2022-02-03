e0bdbf5310b332dc2917de0b80f98df81f52b5f5
dark1922 <jvdematos004@gmail.com>
2022-02-03 18:13:09 -0300

    feath: criação do projeto.

---
+++ projeto-cep/src/main/java/com/projeto/cep/ProjetoCepApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.cep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCepApplication.class, args);
+	}
+
+}
---
+++ projeto-cep/src/test/java/com/projeto/cep/ProjetoCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.cep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
