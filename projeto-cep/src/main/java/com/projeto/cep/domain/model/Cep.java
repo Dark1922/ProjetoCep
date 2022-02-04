c82188ea219e4e7108f57f3217931008c45e42b1
dark1922 <jvdematos004@gmail.com>
2022-02-04 14:57:18 -0300

    feath: tratando mensagens do cep swagger.

--- projeto-cep/src/main/java/com/projeto/cep/domain/model/Cep.java
+++ projeto-cep/src/main/java/com/projeto/cep/domain/model/Cep.java
@@ -1,5 +1,8 @@
 package com.projeto.cep.domain.model;
 
+import javax.validation.constraints.NotBlank;
+
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
@@ -8,15 +11,36 @@ public class Cep {
 	
 	
 	/*dados de consulta cep*/
+	
+	@NotBlank
+	@ApiModelProperty(example = "15230-000",position = 5, name = "Digite um cep válido") 
 	private String cep;
+	
+	@ApiModelProperty(example = "Rua Bernati",position = 10) 
 	private String logradouro;
+	
+	@ApiModelProperty(example = "ap 204",position = 15) 
 	private String complemento;
+	
+	@ApiModelProperty(example = "são carlos",position = 20) 
 	private String bairro;
+	
+	@ApiModelProperty(example = "Adolfo",position = 25) 
 	private String localidade;
+	
+	@ApiModelProperty(example = "SP",position = 30) 
 	private String uf;
+	
+	@ApiModelProperty(example = "3500204",position = 35) 
 	private String ibge;
+	
+	@ApiModelProperty(example = "1510",position = 40) 
 	private String gia;
+	
+	@ApiModelProperty(example = "17",position = 45) 
 	private String ddd;
+	
+	@ApiModelProperty(example = "6103",position = 50) 
 	private String siafi;
 	
 }
